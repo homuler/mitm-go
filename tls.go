694b74f5cd32c4b2259cb98941c6b232cae7edde
Junrou Nishida <homuler@users.noreply.github.com>
2024-03-18 00:21:00 +0900

    refactor!: Accept always returns tls.Server

--- tls.go
+++ tls.go
@@ -134,8 +134,8 @@ func (tl *tlsListener) Accept() (net.Conn, error) {
 
 	tlsConn, err := newTLSServer(conn, tl.config, tl.serverInfoCache)
 	if err != nil {
-		_ = conn.Close()
-		return nil, err
+		// TODO: log the error
+		fmt.Printf("MITM %v\n", err)
 	}
 	return tlsConn, nil
 }
@@ -145,11 +145,13 @@ func (tl *tlsListener) Addr() net.Addr { return tl.listener.Addr() }
 
 var (
 	ErrPeekClientHello     = errors.New("failed to peek client hello")
-	ErrHandshakeWithServer = errors.New("failed to handshake with the server")
+	errHandshakeWithServer = errors.New("failed to handshake with the true server")
+	errForgeCertificate    = errors.New("failed to forge a certificate")
 )
 
 type tlsConn struct {
-	conn   net.Conn
+	conn net.Conn
+
 	reader *memorizingReader
 	config *TLSConfig
 }
@@ -162,36 +164,45 @@ func NewTLSServer(conn net.Conn, config *TLSConfig, serverInfoCache ServerInfoCa
 	return newTLSServer(conn, config.Normalize(), serverInfoCache)
 }
 
+// newTLSServer returns a TLS server connection and the error that occurred while forging a certificate if any.
+// tls.Conn is always returned even if an error occurred.
 func newTLSServer(conn net.Conn, config *TLSConfig, serverInfoCache ServerInfoCache) (*tls.Conn, error) {
 	bufPtr := defaultBufferPool.Get().(*[]byte)
-	c := newTlsConn(conn, config, (*bufPtr)[0:0])
-	clientHello := &clientHelloMsg{}
+	tlsConn := newTlsConn(conn, config, (*bufPtr)[0:0])
+	closeTLSConn := func() (err error) {
+		err = tlsConn.conn.Close()
+		*bufPtr = tlsConn.reader.buf
+		defaultBufferPool.Put(bufPtr)
+		return
+	}
 
-	err := peekClientHello(c.reader, clientHello)
+	clientHello := &clientHelloMsg{}
+	err := tlsConn.readClientHello(clientHello)
+	// NOTE: we have peeked the ClientHello message and won't peek more data from the connection.
+	{
+		// this must not fail
+		_, err := tlsConn.reader.Seek(0, io.SeekStart)
+		if err != nil {
+			panic(err)
+		}
+	}
 	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrPeekClientHello, err)
+		// let tls.Server to handle the error
+		c := NewTamperedConn(tlsConn.conn,
+			TamperConnRead(tlsConn.reader.OneTimeReader().Read),
+			TamperConnClose(closeTLSConn))
+		return tls.Server(c, nil), err
 	}
 
 	dstAddr := config.GetDestination(conn, clientHello.serverName)
-	cert, protocol, err := c.handshakeWithServer(dstAddr, clientHello, serverInfoCache)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %w", ErrHandshakeWithServer, err)
-	}
+	proxyConn := NewProxyConn(tlsConn.conn, dstAddr,
+		TamperConnRead(tlsConn.reader.OneTimeReader().Read),
+		TamperConnClose(closeTLSConn))
 
-	serverConfig := c.config.GetServerConfig(cert, protocol)
-
-	_, err = c.reader.Seek(0, io.SeekStart)
-	if err != nil {
-		return nil, fmt.Errorf("failed to seek the reader: %w", err)
-	}
-	proxyConn := NewProxyConn(c.conn, dstAddr,
-		TamperConnRead(c.reader.OneTimeReader().Read),
-		TamperConnClose(func() error {
-			*bufPtr = c.reader.buf
-			defaultBufferPool.Put(bufPtr)
-			return nil
-		}))
-	return tls.Server(proxyConn, serverConfig), nil
+	cert, protocol, err := tlsConn.handshakeWithServer(dstAddr, clientHello, serverInfoCache)
+	serverConfig := tlsConn.config.GetServerConfig(cert, protocol)
+	// let tls.Server to handle the error
+	return tls.Server(proxyConn, serverConfig), err
 }
 
 func newTlsConn(conn net.Conn, config *TLSConfig, buffer []byte) *tlsConn {
@@ -204,6 +215,7 @@ func newTlsConn(conn net.Conn, config *TLSConfig, buffer []byte) *tlsConn {
 
 // handshakeWithServer returns a certificate of the server and the negotiated protocol.
 // serverInfoCache will be updated with the result.
+// Note that even if the error in the return value is not nil, other return values may not be zero values.
 func (c *tlsConn) handshakeWithServer(dstAddr net.Addr, msg *clientHelloMsg, serverInfoCache ServerInfoCache) (*tls.Certificate, string, error) {
 	serverName := msg.serverName
 	alpnProtocols := msg.alpnProtocols
@@ -242,24 +254,20 @@ func (c *tlsConn) handshakeWithServer(dstAddr net.Addr, msg *clientHelloMsg, ser
 
 	tc, err := tls.Dial(dstAddr.Network(), dstAddr.String(), clientConfig)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to dial %v(%v): %w", serverName, dstAddr, err)
+		return si.certificate, "", fmt.Errorf("%w (serverName=%v, addr=%v): %w", errHandshakeWithServer, serverName, dstAddr, err)
 	}
 	state := tc.ConnectionState()
+	negotiatedProtocol := state.NegotiatedProtocol
 
 	if si.certificate == nil {
 		certs := state.PeerCertificates
-		if len(certs) == 0 {
-			// it should not occur.
-			return nil, "", fmt.Errorf("no certificates of %v(%v) found", serverName, dstAddr)
-		}
 		cert, err := ForgeCertificate(c.config.RootCertificate, certs[0])
 		if err != nil {
-			return nil, "", fmt.Errorf("failed to forge a certificate of %v(%v): %w", serverName, dstAddr, err)
+			return nil, negotiatedProtocol, fmt.Errorf("%s (serverName=%v, addr=%v): %w", errForgeCertificate, serverName, dstAddr, err)
 		}
 		si.certificate = &cert
 	}
 
-	negotiatedProtocol := state.NegotiatedProtocol
 	for _, p := range clientConfig.NextProtos {
 		if p == negotiatedProtocol {
 			break
@@ -269,16 +277,15 @@ func (c *tlsConn) handshakeWithServer(dstAddr net.Addr, msg *clientHelloMsg, ser
 	si.protocols[negotiatedProtocol] = true
 	serverInfoCache[serverName] = si
 
-	return si.certificate, state.NegotiatedProtocol, nil
+	return si.certificate, negotiatedProtocol, nil
 }
 
 var (
-	errUnexpectedRecordType = errors.New("unexpected record type")
-	errInvalidClientHello   = errors.New("invalid ClientHello")
+	errInvalidClientHello = errors.New("invalid ClientHello")
 )
 
-func peekClientHello(r *memorizingReader, msg *clientHelloMsg) error {
-	hdr, err := r.Next(recordHeaderLen)
+func (c *tlsConn) readClientHello(msg *clientHelloMsg) error {
+	hdr, err := c.reader.Next(recordHeaderLen)
 	if err != nil {
 		return err
 	}
@@ -287,11 +294,11 @@ func peekClientHello(r *memorizingReader, msg *clientHelloMsg) error {
 	ver := uint16(hdr[1])<<8 | uint16(hdr[2])
 
 	if typ != recordTypeHandshake || ver > 0x1000 {
-		return fmt.Errorf("%w: type=%v, ver=%v", errUnexpectedRecordType, typ, ver)
+		return fmt.Errorf("unexpected record type: type=%v, ver=%v", typ, ver)
 	}
 	n := int(hdr[3])<<8 | int(hdr[4])
 
-	fragment, err := r.Next(n)
+	fragment, err := c.reader.Next(n)
 	if err != nil {
 		return fmt.Errorf("failed to read the fragment: %w", err)
 	}
