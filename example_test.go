27afec797149732907709096a0cc214205b3efe6
Junrou Nishida <homuler@users.noreply.github.com>
2024-05-19 18:36:44 +0900

    test: add TLSServer examples

--- example_test.go
+++ example_test.go
@@ -2,6 +2,9 @@ package mitm_test
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +12,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/homuler/mitm-go"
 )
@@ -111,4 +115,212 @@ func ExampleOneTimeListener() {
 	wg.Wait()
 	// Output:
 	// Hello
+}
+
+func mustLoadCACert(cn string) *tls.Certificate {
+	cert, err := mitm.CreateCACert(pkix.Name{CommonName: cn}, 1*time.Hour)
+	if err != nil {
+		panic(err)
+	}
+	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+	return &cert
+}
+
+func mustIssueLocalCert(ca *tls.Certificate, cn string) *tls.Certificate {
+	cert, err := mitm.ForgeCertificate(ca, &x509.Certificate{
+		Subject:     pkix.Name{CommonName: cn},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().Add(1 * time.Hour),
+		DNSNames:    []string{cn},
+		IPAddresses: []net.IP{net.ParseIP("127.0.0.1")},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return &cert
+}
+
+func ExampleNewTLSListener() {
+	rootCACert := mustLoadCACert("root")
+	mitmCACert := mustLoadCACert("mitm")
+
+	defaultPool := x509.NewCertPool()
+	defaultPool.AddCert(rootCACert.Leaf)
+
+	mitmPool := x509.NewCertPool()
+	mitmPool.AddCert(mitmCACert.Leaf)
+
+	// true server
+	l1, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	trueCert := mustIssueLocalCert(rootCACert, "echo")
+	l1 = tls.NewListener(l1, &tls.Config{
+		Certificates: []tls.Certificate{*trueCert},
+	})
+	defer l1.Close()
+
+	go func() {
+		for {
+			conn, err := l1.Accept()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				defer conn.Close()
+				io.Copy(conn, conn) // echo
+			}()
+		}
+	}()
+
+	// MITM server
+	l2, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+
+	l2, err = mitm.NewTLSListener(l2, &mitm.TLSConfig{
+		RootCertificate: mitmCACert, // self-signed certificate
+		GetClientConfig: func(serverName string, alpnProtocols []string) *tls.Config {
+			c := mitm.DefaultGetTLSClientConfig(serverName, alpnProtocols)
+			c.RootCAs = defaultPool
+			return c
+		},
+		GetDestination: func(conn net.Conn, serverName string) net.Addr {
+			return l1.Addr()
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l2.Close()
+
+	go func() {
+		for {
+			conn, err := l2.Accept()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				defer conn.Close()
+
+				io.Copy(os.Stdout, conn)
+				conn.Write([]byte("Bye!\n"))
+			}()
+		}
+	}()
+
+	clientConn, err := tls.Dial(l2.Addr().Network(), l2.Addr().String(), &tls.Config{
+		ServerName: "echo",
+		RootCAs:    mitmPool,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer clientConn.Close()
+
+	clientConn.Write([]byte("Hello, World!\n"))
+	clientConn.CloseWrite()
+	io.Copy(os.Stdout, clientConn)
+	// Output:
+	// Hello, World!
+	// Bye!
+}
+
+func ExampleNewTLSServer() {
+	rootCACert := mustLoadCACert("root")
+	mitmCACert := mustLoadCACert("mitm")
+
+	defaultPool := x509.NewCertPool()
+	defaultPool.AddCert(rootCACert.Leaf)
+
+	mitmPool := x509.NewCertPool()
+	mitmPool.AddCert(mitmCACert.Leaf)
+
+	// true server
+	l1, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	trueCert := mustIssueLocalCert(rootCACert, "echo")
+	l1 = tls.NewListener(l1, &tls.Config{
+		Certificates: []tls.Certificate{*trueCert},
+	})
+	defer l1.Close()
+
+	go func() {
+		for {
+			conn, err := l1.Accept()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				defer conn.Close()
+				io.Copy(conn, conn) // echo
+			}()
+		}
+	}()
+
+	// MITM server
+	l2, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	defer l2.Close()
+
+	go func() {
+		for {
+			conn, err := l2.Accept()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				defer conn.Close()
+
+				tlsConn, err := mitm.NewTLSServer(conn, &mitm.TLSConfig{
+					RootCertificate: mitmCACert, // self-signed certificate
+					GetClientConfig: func(serverName string, alpnProtocols []string) *tls.Config {
+						c := mitm.DefaultGetTLSClientConfig(serverName, alpnProtocols)
+						c.RootCAs = defaultPool
+						return c
+					},
+					GetDestination: func(conn net.Conn, serverName string) net.Addr {
+						return l1.Addr()
+					},
+				})
+				if err != nil {
+					return
+				}
+				defer tlsConn.Close()
+
+				io.Copy(os.Stdout, tlsConn)
+				tlsConn.Write([]byte("Bye!\n"))
+			}()
+		}
+	}()
+
+	clientConn, err := tls.Dial(l2.Addr().Network(), l2.Addr().String(), &tls.Config{
+		ServerName: "echo",
+		RootCAs:    mitmPool,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer clientConn.Close()
+
+	clientConn.Write([]byte("Hello, World!\n"))
+	clientConn.CloseWrite()
+	io.Copy(os.Stdout, clientConn)
+	// Output:
+	// Hello, World!
+	// Bye!
 }
